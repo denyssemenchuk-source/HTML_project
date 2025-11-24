bb56e097d5cafaf2e355a605d2e051ae64bbbc1a
myIndex <231722259+denyssemenchuk-source@users.noreply.github.com>
2025-11-24 20:41:03 +0200

    Lab4-5

--- vue-lab/src/main.ts
+++ vue-lab/src/main.ts
@@ -1,11 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // Якщо у вас є роутер
+import "./assets/index.css"; // Лише наш Tailwind файл!
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
--- vue-lab/src/views/ContactView.vue
+++ vue-lab/src/views/ContactView.vue
@@ -4,9 +4,11 @@ import UserCard from '@/components/UserCard.vue';
 </script>
 
 <template>
-  <div class="contact-page">
-    <h1>Сторінка контактів</h1>
-    <p>Це наша нова сторінка контактів!</p>
+  div class="p-5">
+  <h1 class="text-2xl font-bold mb-4 text-gray-800">Сторінка контактів</h1>
+  <p class="mb-6 text-gray-600">Це наша нова сторінка контактів!</p>
+
+  <div class="grid gap-4 max-w-md">
 
     <UserCard username="Student_1" :isActive="true" />
     <UserCard username="Student_2" :isActive="false" />
